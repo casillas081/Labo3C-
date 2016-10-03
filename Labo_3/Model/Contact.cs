5fbdd6ed88b5ee8b0fd40d17490615c779f3eb21
casillas081 <christ.schepmans@hotmail.com>
2016-10-03 10:08:12 +0200

    Fin Labo3

---
+++ Labo_3/Model/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Email { get; set; }
+
+        public Contact(String fN, String lN, String eM)
+        {
+            FirstName = fN;
+            LastName = lN;
+            Email = eM;        
+        }
+    }
+}
